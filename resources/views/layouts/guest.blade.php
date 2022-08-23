298de91d9cee840b95f02cf333efb265ae356e5c
Omar630603 <omar.yem1111@gmail.com>
2022-08-23 15:55:13 +0700

    Edit layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,9 +18,22 @@
         <link rel="icon" href="{{ asset('../logo.png') }}">
     </head>
 
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation-guest')
+
+            <!-- Page Heading -->
+            @if (isset($header))
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+            @endif
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
         </div>
     </body>
 
